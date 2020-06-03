0672aeefa60abeec9e1cdafc989c6b9223d8bfc4
jefferson.souza <jefferson.souza@synchro.com.br>
2020-06-03 14:11:44 -0300

    docz setup

---
+++ .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/3365790a185101a8d11a1f3d1fed476a.js
@@ -0,0 +1,9 @@
+import { Props, Playground } from 'docz';
+import MaskInput from "../../../../../src/MaskInput.tsx";
+import * as React from 'react';
+export default {
+  Props,
+  Playground,
+  MaskInput,
+  React
+};
