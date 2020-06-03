cbae2181ccacff047f9399816b55d9b5284aac3b
jefferson.souza <jefferson.souza@synchro.com.br>
2020-06-03 14:23:04 -0300

    update docs

--- .docz/.cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---dist-readme-md": () => import("./../../dist/README.md" /* webpackChunkName: "component---dist-readme-md" */),
-  "component---docs-mdx": () => import("./../../docs.mdx" /* webpackChunkName: "component---docs-mdx" */),
-  "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */)
-}
-
--- .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/3365790a185101a8d11a1f3d1fed476a.js
+++
@@ -1,9 +0,0 @@
-import { Props, Playground } from 'docz';
-import MaskInput from "../../../../../src/MaskInput.tsx";
-import * as React from 'react';
-export default {
-  Props,
-  Playground,
-  MaskInput,
-  React
-};
